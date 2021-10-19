1ba46dc48f95ad279d170f1d60495599ddd2c66a
SakataWolf <87884401+SakataWolf@users.noreply.github.com>
2021-10-19 17:48:37 -0300

    Add files via upload

---
+++ Quantidade de números impares/Quantidade de números impares/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Quantidade_de_números_impares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("quantidade de números ímpares.\n");
+            Console.Write("Digite um número: ");
+            int a, b, i = 0, mn, Mn;
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Digite outro número: ");
+            b = int.Parse(Console.ReadLine());
+            if (a <= b)
+            {
+                mn = a;
+                Mn = b;
+            }
+            else
+            {
+                mn = b;
+                Mn = a;
+            }
+            for (int x = mn; x <= Mn; x++)
+            {
+                if (x % 2 != 0) { i++; }
+            }
+            Console.WriteLine("Quantidade de números ímpares entre {0} e {1} é {2}", mn, Mn, i);
+        }
+    }
+}
